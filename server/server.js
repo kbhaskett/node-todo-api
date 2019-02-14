b81f967d0c4e0a4bdd6d8ca2c251559bcf6a286d
Kevin Haskett <Kevin.Haskett@siriuscom.com>
2019-02-14 16:54:28 -0600

    GET by Id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var ObjectId = require('mongoose').Types.ObjectId;
+
 
 var {mongoose} = require('../db/mongoose');
 var {Todo} = require('../models/todo');
@@ -15,19 +17,36 @@ app.post('/todo', (req, res) => {
     });
 
     todo.save().then((doc) => {
-        res.status(201).send(doc);
+        return res.status(201).send(doc);
     }, (err) => {
-        res.status(400).send(err);
+        return res.status(400).send(err);
     });
 });
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) =>{
-        res.send({todos});
+        return res.send({todos});
     }, (e) => {
-        res.status(400).send(e);
+        return res.status(400).send(e);
     })
 })
+
+app.get('/todo/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+        return res.status(400).send('Invalid Id passed');
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        } else {
+            return res.send(todo);
+        }
+    }, (err) => {
+        return res.status(400).send('Problems retrieving this item');
+    });
+});
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
