4c4b7087bcc77bdcc0021f517e8dff348f5aa0aa
Kevin Haskett <Kevin.Haskett@siriuscom.com>
2019-02-20 11:11:39 -0600

    adding seed

---
+++ tests/seed/seed.js
@@ -0,0 +1,47 @@
+const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
+const {Todo} = require('../../models/todo');
+const {User} = require('../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+const users = [{
+    _id: userOneId,
+    email: 'user1pass@example.com',
+    password: 'user1pass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({_id: userOneId, access: 'auth'}, 'hockeysticks').toString()
+    }]
+},
+{
+    _id: userTwoId,
+    email: 'user2pass@example.com',
+    password: 'user2pass'
+}];
+const populateUsers = (done) => {
+    User.deleteMany({}).then(()=> {
+        var userOne = new User(users[0]).save();
+        var userTwo = new User(users[1]).save();
+        return Promise.all([userOne, userTwo]);
+    }).then(() => done());
+};
+
+const todos = [{
+    _id: new ObjectID(),
+    text: 'First test todo',
+  }, {
+    _id: new ObjectID(),
+    text: 'Second test todo',
+    completed: true,
+    completedAt: new Date()
+  }];
+  
+const populateTodos = (done) => {
+    Todo.deleteMany({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
+};
+
+module.exports = {todos, populateTodos, users, populateUsers};
--- tests/server.test.js
+++ tests/server.test.js
@@ -4,22 +4,10 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('../server/server');
 const {Todo} = require('../models/todo');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-const todos = [{
-  _id: new ObjectID(),
-  text: 'First test todo',
-}, {
-  _id: new ObjectID(),
-  text: 'Second test todo',
-  completed: true,
-  completedAt: new Date()
-}];
-
-beforeEach((done) => {
-  Todo.deleteMany({}).then(() => {
-    return Todo.insertMany(todos);
-  }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todo', () => {
   it('should create a new todo', (done) => {
